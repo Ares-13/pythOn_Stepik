47e6e9446f6d5ce08d8dc2077a38c0193ae8e628
Nikita Sabirov <nikita.sabirov.30@gmail.com>
2026-01-06 17:42:29 +0300

    add selection sort

---
+++ grokaem_algor/selectionSort.py
@@ -0,0 +1,21 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    
+    return smallest_index
+
+
+def selectionSort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        new_arr.append(arr.pop(smallest)) # Из исходного массива удаляем элемент с наименьшим индексом и добавляем его в новый массив
+    
+    return new_arr
+
+
+print(selectionSort([1,5, -4, 10, 7]))
